4f9731d24432517e876d67b46c9fe678cf762811
isaall7 <adefaisalndut@gmail.com>
2025-09-09 08:06:59 +0700

    sislab project

--- app/Http/Controllers/RuanganController.php
+++ app/Http/Controllers/RuanganController.php
@@ -91,7 +91,7 @@ class RuanganController extends Controller
         $ruangan = Ruangan::findOrFail($id);
 
         // Validasi: hanya bisa dihapus kalau status "Tersedia"
-        if ($ruangan->status !== 'tersedia') {
+        if ($ruangan->status !== 'Tersedia') {
             Alert::error('Gagal', 'Ruangan masih dipinjam dan tidak dapat dihapus');
             return redirect()->route('ruangan.index');
         }else{
